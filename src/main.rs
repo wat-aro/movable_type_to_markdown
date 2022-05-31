caa68c55b625f100ee4e4a35978f6e683adb7455
wat-aro <kazutas1008@gmail.com>
2022-05-31 23:46:46 +0900

    Add arguments

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
-use clap::Command;
+use clap::{Arg, Command};
 
 fn main() {
-    Command::new("Movable type to markdown")
+    let command = Command::new("Movable type to markdown")
         .author("wat-aro")
         .version("0.1.0")
         .about("Convert to markdown from movable type")
+        .arg(
+            Arg::new("FILE")
+                .required(true)
+                .help("Target movable type file location."),
+        )
+        .arg(
+            Arg::new("DIRECTORY")
+                .required(true)
+                .help("Output directory"),
+        )
         .get_matches();
 
-    println!("hello");
+    println!("FILE: {:?}", command.value_of("FILE"));
+    println!("DIRECTORY: {:?}", command.value_of("DIRECTORY"));
 }
