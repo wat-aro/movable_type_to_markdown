72f0f3266bec453096ff529d91d92598a00e25ea
wat-aro <kazutas1008@gmail.com>
2022-06-01 23:17:42 +0900

    Add MetaData

--- src/movable_type.rs
+++ src/movable_type.rs
@@ -1,5 +1,26 @@
+use std::error::Error;
+
+use chrono::{DateTime, FixedOffset};
+use nom::{
+    bytes::streaming::tag, character::complete::anychar, combinator::recognize, multi::many1,
+    sequence::preceded, IResult,
+};
+
 #[derive(Debug, PartialEq)]
-pub struct Post {}
+pub struct Post {
+    meta_data: MetaData,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct MetaData {
+    author: String,
+    title: String,
+    basename: String,
+    allow_comments: bool,
+    convert_breaks: bool,
+    date: DateTime<FixedOffset>,
+    image: String,
+}
 
 pub fn parse(_text: &str) -> Vec<Post> {
     vec![Post {}]
